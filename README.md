dcd8bc7404bab909fb94681ebbd9090eb5dc0dd5
Samuel Newhouse <samuel_newhouse@yahoo.com>
2024-03-23 10:21:03 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This is for Personal Project 1 on boot.dev
+
+This is a manager type game that's based on rock, paper, scissors.
---
+++ script.js
@@ -0,0 +1,19 @@
+class Player {
+	type = ""
+	health = 10
+	defense = 2
+	attack = 2
+	speed = 2
+}
+
+class Rock extends Player {
+	type = "🪨"
+}
+
+class Paper extends Player {
+	type = "📜"
+}
+
+class Scissors extends Player {
+	type = "✂️"
+}
