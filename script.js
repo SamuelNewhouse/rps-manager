d392ac20f650af9c4de0c3f9a474ae3da5fbe4d4
Samuel Newhouse <samuel_newhouse@yahoo.com>
2024-03-23 12:26:08 -0500

    Get basic lane display working

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+const laneWidth = 7;
+const laneHeight = 3;
+const rockType = "🪨"
+const paperType = "📜"
+const scissorsType = "✂️"
+
+const getLaneCSSClassFromType = type => {
+	if (type === rockType) return 'rock-lane'
+	if (type === paperType) return 'paper-lane'
+	if (type === scissorsType) return 'scissors-lane'
+	throw new Error("Invalid type arument in getCSSFromType")
+}
+
 class Player {
 	type = ""
 	health = 10
@@ -7,13 +20,37 @@ class Player {
 }
 
 class Rock extends Player {
-	type = "🪨"
+	type = rockType
 }
 
 class Paper extends Player {
-	type = "📜"
+	type = paperType
 }
 
 class Scissors extends Player {
-	type = "✂️"
+	type = scissorsType
 }
+
+class Lane {
+	constructor(type) {
+		if (type === rockType || type === paperType || type === scissorsType)
+			this.type = type
+		else
+			throw new Error('Invalid type argument in Lane constructor.')
+	}
+
+	createLaneDOMElement = () => {
+		const laneElement = document.createElement("div")
+		laneElement.classList.add(getLaneCSSClassFromType(this.type), 'lane')
+		for(let i = 0; i < laneWidth; i++) {
+			const cellElement = document.createElement("span")
+			cellElement.classList.add('lane-cell')
+			laneElement.appendChild(cellElement)
+		}
+		return laneElement
+	}
+}
+
+const newLane = new Lane(rockType)
+const playField = document.getElementById('playField')
+playField.appendChild(newLane.createLaneDOMElement())
